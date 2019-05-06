5de4ea9107bca9447102bcd4c9ecdb360e054a76
temightyspoon <44456520+temightyspoon@users.noreply.github.com>
2019-05-06 12:24:27 +1200

    Update Base Setup.sh

--- Base Setup.sh
+++ Base Setup.sh
@@ -4,4 +4,4 @@
 sudo apt update -y
 sudo apt upgrade -y
 
-sudo apt install neofetch ssh htop chromium-browser vlc git net-tools curl -y
+sudo apt install neofetch ssh htop chromium-browser vlc git net-tools curl wget unzip -y
