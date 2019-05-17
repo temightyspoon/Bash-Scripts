299d39a046efd501971700e0072867a8ce996011
temightyspoon <44456520+temightyspoon@users.noreply.github.com>
2019-05-17 13:20:22 +1200

    Updated - added in ownership and linking

--- LAMP_install.sh
+++ LAMP_install.sh
@@ -6,6 +6,7 @@ sudo apt update
 
 #installs the stack
 sudo apt install apache2 mariadb-server php php-mysql libapache2-mod-php php-cli
+sudo a2enmod rewrite
 
 #enables the webserver on startup
 sudo systemctl enable apache2
@@ -24,3 +25,19 @@ sudo echo "<?php phpinfo(); ?>" > /var/www/html/info.php
 
 #optional- Install phpmyadmin
 sudo apt install phpmyadmin php-mbstring php-gettext
+sudo php5enmod mcrypt
+
+sudo service apache2 restart
+
+#takes ownership of files
+sudo chown root:root /etc/phpmyadmin/ -R
+sudo chown root:www-data /etc/phpmyadmin/config-db.php
+sudo chown root:www-data /etc/phpmyadmin/htpasswd.setup
+
+#links files to make editing config easier
+sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf.d/phpmyadmin.conf
+sudo /etc/init.d/apache2 reload
+sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-enabled/phpmyadmin.conf
+sudo /etc/init.d/apache2 reload
+sudo service apache2 restart
+clear
