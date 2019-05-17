6436bb86aaaaef8afa77b7563616080708396266
temightyspoon <ark@gmail.com>
2019-05-17 12:33:59 +1200

    updated docker start

--- docker_install.sh
+++ docker_install.sh
@@ -1,15 +1,22 @@
-#!bin/bash
+#!/bin/bash
 sudo apt update
 
+#installs needed commands
 sudo apt install apt-transport-https ca-certificates curl software-properties-common
 
+#pulls key and adds docker repo
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
+#updates apt repo and installs docker CE
 sudo apt update
-sudo apt install docker-ce
+sudo apt install docker-ce -y
 sudo usermod -aG docker $USER
+clear
+
+#displays docker version 
 docker -v
 
-echo "press any key to run your first docker"
-docker container run hello-world
+#waits for key press before running hello-world
+read -n 1 -s -r -p "Press any key to run your first docker"
+sudo docker container run hello-world
