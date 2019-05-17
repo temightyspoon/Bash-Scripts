d2cca1288516b3da9464a0372697632d84327d26
temightyspoon <44456520+temightyspoon@users.noreply.github.com>
2019-05-17 13:16:33 +1200

    Added in Atom and VScode

--- Base_Setup.sh
+++ Base_Setup.sh
@@ -7,11 +7,19 @@ read $usename
 echo "please enter email for git"
 read $email_addr
 
+sudo add-apt-repository -y ppa:webupd8team/atom
+
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt autoremove
 
-sudo apt install neofetch ssh htop chromium-browser vlc git net-tools curl wget unzip filezilla -y
+sudo apt install neofetch ssh htop chromium-browser vlc git net-tools curl wget unzip filezilla atom -y
+
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg;
+  sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg;
+	sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list';
+	sudo apt-get update;
+	sudo apt-get install code;
 
 git config --global user.name $username
 git config --global user.email $email_addr
