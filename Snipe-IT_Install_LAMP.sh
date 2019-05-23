f79f9e5dfc1bd1e33fae2d7791b172cbd9667796
temightyspoon <44456520+temightyspoon@users.noreply.github.com>
2019-05-24 10:30:55 +1200

    Corrected some formatting

--- Snipe-IT_Install_LAMP.sh
+++ Snipe-IT_Install_LAMP.sh
@@ -17,7 +17,7 @@ readonly APP_USER="snipeit_user"
 readonly APP_NAME="snipe-it"
 readonly APP_PATH="/var/www/$APP_NAME"
 
-#DATABASE CREATION
+#DATABASE VARIABLE INPUT
 	read -p "Please enter a Database name [snipe-it_db]: " DB_NAME
   DB_NAME=${DB_NAME:-snipe-it_db}
   read -p "Please enter a Database user [snipe-it_user]: " DB_USER
@@ -25,29 +25,28 @@ readonly APP_PATH="/var/www/$APP_NAME"
   read -p "Please enter a PASSWORD to be used for the snipeit database user!" DB_PASS
   read -p "Please enter root user MySQL password!" rootpasswd
 
-mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${DB_NAME};"
-mysql -uroot -p${rootpasswd} -e "show databases;"
-mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON snipeit_db.* TO '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';"
-mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
+#DATABASE VARIABLE INPUT
+  mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${DB_NAME};"
+  mysql -uroot -p${rootpasswd} -e "show databases;"
+  mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON snipeit_db.* TO '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';"
+  mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
   echo "Database Creation Complete"
   echo " Database Name:	 ${DB_NAME}"
   echo " Username:    	 ${DB_USER}"
 
-
 #Install composer - A dependancy manager for PHP
-cd /tmp
-curl -sS https://getcomposer.org/installer | php
-mv composer.phar /usr/local/bin/composer
+  cd /tmp
+  curl -sS https://getcomposer.org/installer | php
+  mv composer.phar /usr/local/bin/composer
 
 
 #clones from repository
-git clone https://github.com/snipe/snipe-it $APP_PATH
+  git clone https://github.com/snipe/snipe-it $APP_PATH
 
-#adds ther snipeitapp user
-adduser --quiet --disabled-password --gecos '""' "$APP_USER"
-
-chmod -R 775 "$APP_PATH/storage"
-chmod -R 755 "$APP_PATH/public/uploads"
+#adds ther snipeitapp user and sets permission levels
+  adduser --quiet --disabled-password --gecos '""' "$APP_USER"
+  chmod -R 775 "$APP_PATH/storage"
+  chmod -R 755 "$APP_PATH/public/uploads"
 
 #grants permssions to snipeitapp user
 chown -R "$APP_USER":"$apache_group" "$APP_PATH"
@@ -60,7 +59,6 @@ $ sudo cp .env.example .env
 tzone=$(cat /etc/timezone)
 read -p "Please enter the URL for site [localhost]: " WEB_ADDR
   WEB_ADDR=${WEB_ADDR:-localhost}
-
 
   sed -i '1 i\#Created By Snipe-it Installer' "$APP_PATH/.env"
   sed -i "s|^\\(APP_TIMEZONE=\\).*|\\1$tzone|" "$APP_PATH/.env"
@@ -75,4 +73,3 @@ phpenmod mcrypt
 phpenmod mbstring
 a2enmod rewrite
 #a2ensite $APP_NAME.conf
-
