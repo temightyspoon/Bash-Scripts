6f0968cc89ab87468828dbfc8f41212f3451f4ff
temightyspoon <44456520+temightyspoon@users.noreply.github.com>
2019-05-24 10:27:34 +1200

    Added in sed commands for .env file

--- Snipe-IT_Install_LAMP.sh
+++ Snipe-IT_Install_LAMP.sh
@@ -12,22 +12,26 @@ apt install php-mcrypt php-curl php-mysql php-gd php-ldap php-zip php-mbstring p
 #enable webserver on startup
 systemctl start mariadb.service
 
+#set variables for user, app name and path,
 readonly APP_USER="snipeit_user"
 readonly APP_NAME="snipe-it"
 readonly APP_PATH="/var/www/$APP_NAME"
 
 #DATABASE CREATION
-	read -p "Please enter root user MySQL password!" rootpasswd
+	read -p "Please enter a Database name [snipe-it_db]: " DB_NAME
+  DB_NAME=${DB_NAME:-snipe-it_db}
+  read -p "Please enter a Database user [snipe-it_user]: " DB_USER
+  DB_USER=${DB_USER:-snipe-it_user}
+  read -p "Please enter a PASSWORD to be used for the snipeit database user!" DB_PASS
+  read -p "Please enter root user MySQL password!" rootpasswd
 
-mysql -uroot -p${rootpasswd} -e "CREATE DATABASE snipeit_db;"
+mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${DB_NAME};"
 mysql -uroot -p${rootpasswd} -e "show databases;"
-	
-	read -p "Please enter a PASSWORD to be used for the snipeit database user!" userpass
-mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON snipeit_db.* TO '$snipeit_user'@'localhost' IDENTIFIED BY '${userpass}';"
+mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON snipeit_db.* TO '${DB_USER}'@'localhost' IDENTIFIED BY '${DB_PASS}';"
 mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
   echo "Database Creation Complete"
-  echo " Database Name:	 snipeitapp"
-  echo " Username:	 snipeit_user"
+  echo " Database Name:	 ${DB_NAME}"
+  echo " Username:    	 ${DB_USER}"
 
 
 #Install composer - A dependancy manager for PHP
@@ -51,6 +55,21 @@ chown -R "$APP_USER":"$apache_group" "$APP_PATH"
 #setup .env file
 cd /var/www/snipe-it
 $ sudo cp .env.example .env
+
+#Set needed variable in .env file
+tzone=$(cat /etc/timezone)
+read -p "Please enter the URL for site [localhost]: " WEB_ADDR
+  WEB_ADDR=${WEB_ADDR:-localhost}
+
+
+  sed -i '1 i\#Created By Snipe-it Installer' "$APP_PATH/.env"
+  sed -i "s|^\\(APP_TIMEZONE=\\).*|\\1$tzone|" "$APP_PATH/.env"
+  sed -i "s|^\\(DB_HOST=\\).*|\\1$localhost|" "$APP_PATH/.env"
+  sed -i "s|^\\(DB_DATABASE=\\).*|\\1$DB_NAME|" "$APP_PATH/.env"
+  sed -i "s|^\\(DB_USERNAME=\\).*|\\1$DB_USER|" "$APP_PATH/.env"
+  sed -i "s|^\\(DB_PASSWORD=\\).*|\\1$DB_PASS|" "$APP_PATH/.env"
+  sed -i "s|^\\(APP_URL=\\).*|\\1http://$WEB_ADDR|" "$APP_PATH/.env"
+
 
 phpenmod mcrypt
 phpenmod mbstring
