12589eecefddd6f417628d8b97f2a8b0f3c19eb3
temightyspoon <bgraaronb@briscoes-nz.co.nz>
2019-05-28 11:47:21 +1200

    Updating commands and extentions

--- Create Database.sh
+++ Create Database.sh
@@ -2,18 +2,15 @@
 
 # If /root/.my.cnf exists then root password isnt needed
 if [ -f /root/.my.cnf ]; then
-	echo "Please enter the NAME of the new database! (example: database1)"
-	read dbname
-		echo "Creating new database..."
+		read -p "Please enter the NAME of the new database!: " dbname
+	echo "Creating new database: $dbname"
 	mysql -e "CREATE DATABASE ${dbname}"
 	echo "Database successfully created!"
 	echo "Showing existing databases..."
 	mysql -e "show databases;"
 	echo ""
-	echo "Please enter the NAME of the new database user! (example: user1)"
-	read username
-	echo "Please enter the PASSWORD for the new database user!"
-	read userpass
+		read -p "Please enter the NAME of the new database user!: " username
+		read -s -p "Please enter the PASSWORD for the new database user!"  userpass
 	echo "Creating new user..."
 	mysql -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
 	echo "User successfully created!"
@@ -26,20 +23,16 @@ if [ -f /root/.my.cnf ]; then
 	
 # If /root/.my.cnf doesn't exist then it will ask for root password	
 else
-	echo "Please enter root user MySQL password!"
-	read rootpasswd
-	echo "Please enter the NAME of the new database! (example: database1)"
-	read dbname
+		read  -s -p "Please enter root user MySQL password!: " rootpasswd
+		read -p "Please enter the NAME of the new database!: " dbname
 	echo "Creating new database..."
 	mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${dbname} /*\!40100 DEFAULT CHARACTER SET ${charset} */;"
 	echo "Database successfully created!"
 	echo "Showing existing databases..."
 	mysql -uroot -p${rootpasswd} -e "show databases;"
 	echo ""
-	echo "Please enter the NAME of the new database user! (example: user1)"
-	read username
-	echo "Please enter the PASSWORD for the new database user!"
-	read userpass
+		read -p "Please enter the NAME of the new database user!: " username
+		read -s -p "Please enter the PASSWORD for the new database user!"  userpass
 	echo "Creating new user..."
 	mysql -uroot -p${rootpasswd} -e "CREATE USER ${username}@localhost IDENTIFIED BY '${userpass}';"
 	echo "User successfully created!"
---
+++ remove Pi Bloat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#script to remove all the bloatware that comes preinstalled on Rasbian.
+
+apt update
+apt remove libreoffice* wolfram* minecraft-pi sonic-pi scratch nuscratch idle3 smartsim  java-common python-minecraftpi python3-minecraftpi bluej nodered claws-mail claws-mail-i18n python-pygame --purge -y
+apt clean
+apt autoremove --purge -y
+apt update
+apt upgrade -y
+apt autoremove -y
